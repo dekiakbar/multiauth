e033843994bb3f2b0f17da306c19a1debcbd5f48
deki <dekiakbar@linuxmail.org>
2017-09-15 00:58:22 +0700

    login admin

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware('guest:admin');
+	}
+
+    public function showLoginForm()
+    {
+    	return view('auth.admin-login');
+    }
+
+    public function login(Request $request)
+    {
+    	//validasi form login
+    	$this->validate($request,[
+    		'email' => 'required|email',
+    		'password' => 'required|min:8'
+    	]);
+
+    	//percobaan login
+    	//
+    	if (Auth::guard('admin')->attempt(['email' => $request->email,'password' => $request->password], $request->remember)) {
+    		//login berhasil dan redirect halaman
+    		return redirect()->intended(route('admin.dashboard'));
+    	} 
+
+    	// gagal login dan redirect
+    	return redirect()->back()->withInput($request->only('email','remember'));
+    }
+}
