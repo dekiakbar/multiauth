a926c8ff0c138696a54386728fa40e5f1e8231d8
deki <dekiakbar@linuxmail.org>
2017-09-15 19:55:17 +0700

    add logout

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -10,7 +10,7 @@ class AdminLoginController extends Controller
 {
 	public function __construct()
 	{
-		$this->middleware('guest:admin');
+		$this->middleware('guest:admin', ['except' => ['logout']]);
 	}
 
     public function showLoginForm()
@@ -36,4 +36,17 @@ class AdminLoginController extends Controller
     	// gagal login dan redirect
     	return redirect()->back()->withInput($request->only('email','remember'));
     }
+
+     /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/');
+    }
+
 }
